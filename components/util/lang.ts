915ef7946eefa4fe25902742990d2c220127a4c4
saif_mohammed1 <sm086189@gmail.com>
2024-09-30 13:55:25 +0300

    upload lang to uk

--- components/util/lang.ts
+++ components/util/lang.ts
@@ -1,15 +1,6 @@
-// "use server";
-// import { cookies } from "next/headers";
-
-// // const getLang = (): Lang | undefined => {
-// //   const langCookie = cookies().get("lang");
-// //   return langCookie ? (langCookie.value as Lang) : undefined;
-// // };
-// // console.log("getLang", getLang);
-
 enum Lang {
   uk = "uk",
   en = "en",
 }
 
-export const lang: Lang = Lang.en;
+export const lang: Lang = Lang.uk;
