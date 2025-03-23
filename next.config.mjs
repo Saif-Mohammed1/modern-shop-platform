76d5b4087179a6a5531890ee981da00ab5d220be
saif_mohammed1 <sm086189@gmail.com>
2025-03-23 17:32:58 +0200

    optomize and upgrade to next15

--- next.config.mjs
+++ next.config.mjs
@@ -23,53 +23,53 @@ const nextConfig = {
       },
     ],
   },
-  // async headers() {
-  //   return [
-  //     {
-  //       source: "/(.*)",
-  //       headers: [
-  //         {
-  //           key: "Strict-Transport-Security",
-  //           value: "max-age=63072000; includeSubDomains; preload",
-  //         },
-  //         {
-  //           key: "Content-Security-Policy",
-  //           value:
-  //             "default-src 'self'; script-src 'self' 'unsafe-inline' *.trusted.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self'; connect-src 'self' *.api.com; frame-src 'none'; object-src 'none'; base-uri 'self'; form-action 'self';",
-  //         },
-  //         {
-  //           key: "X-Content-Type-Options",
-  //           value: "nosniff",
-  //         },
-  //         {
-  //           key: "X-Frame-Options",
-  //           value: "DENY",
-  //         },
-  //         {
-  //           key: "X-XSS-Protection",
-  //           value: "1; mode=block",
-  //         },
-  //         {
-  //           key: "Referrer-Policy",
-  //           value: "strict-origin-when-cross-origin",
-  //         },
-  //         {
-  //           key: "Permissions-Policy",
-  //           value:
-  //             "camera=(), microphone=(), geolocation=(), browsing-topics=()",
-  //         },
-  //         {
-  //           key: "Cross-Origin-Opener-Policy",
-  //           value: "same-origin",
-  //         },
-  //         {
-  //           key: "Cross-Origin-Embedder-Policy",
-  //           value: "require-corp",
-  //         },
-  //       ],
-  //     },
-  //   ];
-  // },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+          // {
+          //   key: "Content-Security-Policy",
+          //   value:
+          //     "default-src 'self'; script-src 'self' 'unsafe-inline' *.trusted.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self'; connect-src 'self' *.api.com; frame-src 'none'; object-src 'none'; base-uri 'self'; form-action 'self';",
+          // },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "Permissions-Policy",
+            value:
+              "camera=(), microphone=(), geolocation=(), browsing-topics=()",
+          },
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 const bundleAnalyzerConfig = withBundleAnalyzer({
