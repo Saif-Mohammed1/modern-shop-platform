abf93245c1e18a933e68c62242a2210318fe9e7c
saif_mohammed1 <saiff.mohammed1@gmail.com>
2025-04-26 01:35:54 +0300

    fix wishlist toggels

--- components/shop/wishList/wishlist.tsx
+++ components/shop/wishList/wishlist.tsx
@@ -1,8 +1,6 @@
 // pages/wishlist.js
 "use client";
-import { useEffect, useState } from "react";
 
-import type { WishlistType } from "@/app/lib/types/wishList.types";
 import { lang } from "@/app/lib/utilities/lang";
 import { useWishlist } from "@/components/providers/context/wishlist/wishlist.context";
 import { accountWishlistTranslate } from "@/public/locales/client/(auth)/account/wishlistTranslate";
@@ -11,25 +9,20 @@ import WishListCard from "./wishListCard";
 
 const WishlistPage = () => {
   const { wishlist } = useWishlist();
-  const [wishlistProduct, setWishlistProduct] =
-    useState<WishlistType>(wishlist);
 
-  useEffect(() => {
-    setWishlistProduct(wishlist);
-  }, [wishlist]);
   return (
     <div className="container mx-auto mt-8 ">
       <h1 className="text-3xl font-bold mb-4 ">
         {accountWishlistTranslate[lang].wishlistPage.title}
       </h1>
       <div className="max-h-screen overflow-y-auto">
-        {wishlistProduct.items.length === 0 ? (
+        {wishlist.items.length === 0 ? (
           <p className="empty">
             {accountWishlistTranslate[lang].wishlistPage.emptyWhishlist}
           </p>
         ) : (
           <div className="grid col gap-4">
-            {wishlistProduct.items.map((product) => {
+            {wishlist.items.map((product) => {
               const products = product.productId;
               return <WishListCard key={products?._id} product={products} />;
             })}
