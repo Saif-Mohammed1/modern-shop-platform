eaeeaf0e654428053ae9540e3f43ce40811d5b85
saif_mohammed1 <saiff.mohammed1@gmail.com>
2025-05-31 10:51:23 +0300

    fix _not-found.. 4rd try

--- next.config.mjs
+++ next.config.mjs
@@ -11,14 +11,7 @@ const nextConfig = {
       static: 600,
     },
   },
-  pageExtensions: [
-    "page.tsx",
-    "page.ts",
-    // FIXME: Next.js has a bug which does not resolve not-found.page.tsx correctly
-    // Instead, use `not-found.ts` as a workaround
-    // "ts" is required to resolve `not-found.ts`
-    // https://github.com/vercel/next.js/issues/65447
-  ],
+
   // config: {
   //   turbopack: {}, // Move `experimental.turbo` to `config.turbopack`
   // },
